d688f127f84bc2e839fd9f00f6b0042a14a89997
psenxiv <320438562+psenxiv@users.noreply.github.com>
2026-09-22 23:07:43 +1000

    fix(ui) pad the point table's outer edges

--- src/Vista.Plugin/Ui/TrackEditorWindow.cs
+++ src/Vista.Plugin/Ui/TrackEditorWindow.cs
@@ -471,7 +471,7 @@ internal sealed unsafe class TrackEditorWindow : Window
         if (ImGui.BeginChild("points", new Vector2(0f, -footer)))
         {
             using var padding = ImRaii.PushStyle(ImGuiStyleVar.CellPadding, CellPadding);
-            if (ImGui.BeginTable("point-table", 6, ImGuiTableFlags.SizingFixedFit | ImGuiTableFlags.RowBg))
+            if (ImGui.BeginTable("point-table", 6, ImGuiTableFlags.SizingFixedFit | ImGuiTableFlags.RowBg | ImGuiTableFlags.PadOuterX))
             {
                 ImGui.TableSetupColumn("#");
                 ImGui.TableSetupColumn("Duration (s)");
