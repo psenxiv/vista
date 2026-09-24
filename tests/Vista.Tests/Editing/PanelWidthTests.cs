1fbdddb4930c534a3e48403fe86e500288b0264c
psenxiv <320438562+psenxiv@users.noreply.github.com>
2026-09-24 10:03:35 +1000

    test(editing) pin panel width limits

---
+++ tests/Vista.Tests/Editing/PanelWidthTests.cs
@@ -0,0 +1,18 @@
+using Vista.Core.Editing;
+using Xunit;
+
+namespace Vista.Tests.Editing;
+
+public class PanelWidthTests
+{
+    [Theory]
+    [InlineData(300f, 300f)]
+    [InlineData(100f, 240f)]
+    [InlineData(0f, 240f)]
+    [InlineData(800f, 600f)]
+    [InlineData(float.NaN, 240f)]
+    public void AWidthIsHeldBetween240And600(float width, float expected)
+    {
+        Assert.Equal(expected, PanelWidth.Clamp(width));
+    }
+}
